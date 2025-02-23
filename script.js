335adcdd038e7b98bd9f27ae447d8a8beba56c29
SEYDOU DIALLO <140073295+douseyx03@users.noreply.github.com>
2025-02-23 23:44:50 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    alert("Bineta, tu es une belle personne ! 🎂 Que cette journée soit remplie de joie et de bonheur. Et que cette nouvelle année soit remplie de bonheur,de success et de joie. Profite🥳.");
+}
